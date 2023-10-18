360407702ac8b73ca299793000c7f86e6a109dc8
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-18 13:54:47 +0300

    set leaf method on zero merkle tree

--- zero_merkle_tree.py
+++ zero_merkle_tree.py
@@ -1,4 +1,5 @@
 import hashlib
+from merkle_tree import MerkleTree
 
 
 class NodeStore:
@@ -48,3 +49,46 @@ class NodeStore:
         - str: Node value.
         """
         return self.nodes.get((level, index), self.zero_hashes[self.height - level])
+
+
+class ZeroMerkleTree(MerkleTree):
+    def __init__(self, height: int):
+        """
+        Args:
+        - height (int): The height of the tree.
+        """
+        self.height = height
+        self.node_store = NodeStore(height)
+
+    def set_leaf(self, index: int, value: str) -> None:
+        """
+        Set a leaf in the Merkle tree and update corresponding nodes on the path of the leaf.
+
+
+        Args:
+        - index (int): The index of the leaf to set.
+        - value (str): The value to set for the leaf.
+        """
+        # Start traversing the leaf's Merkle path at the leaf node.
+        current_index = index
+        current_value = value
+
+        # Don't set the root (level = 0) in the loop, as it has no sibling.
+        for level in range(self.height, 0, -1):
+            # Set the current node in the tree.
+            self.node_store.set(level, current_index, current_value)
+
+            if current_index % 2 == 0:
+                # If the current index is even, then it has a sibling on the right (same level, index = current_index+1).
+                right_sibling = self.node_store.get(level, current_index + 1)
+                current_value = self.hash(current_value, right_sibling)
+            else:
+                # If the current index is odd, then it has a sibling on the left (same level, index = current_index-1).
+                left_sibling = self.node_store.get(level, current_index - 1)
+                current_value = self.hash(left_sibling, current_value)
+
+            # Set current index to the index of the parent node.
+            current_index = current_index // 2
+
+        # Set the root node (level = 0, index = 0) to current value.
+        self.node_store.set(0, 0, current_value)
