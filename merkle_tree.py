b1e81fd41eebc7b4f1ab17cba14655146aa49a39
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-18 04:10:01 +0300

    merkele proof for a give node

--- merkle_tree.py
+++ merkle_tree.py
@@ -1,56 +1,56 @@
 import hashlib
 
 
-def hash_nodes(left_node, right_node):
-    hasher = hashlib.sha256()
-    concatenated_nodes = f"{left_node}{right_node}".encode("utf-8")
-    hasher.update(concatenated_nodes)
-    return hasher.hexdigest()
-
-
-def get_merkle_path_of_node(level, index):
-    """
-    Computes the Merkle path of a node in a Merkle tree.
-
-    When a leaf in a Merkle tree is updated, the change bubbles up the tree
-    affecting all ancestor nodes. These nodes form the Merkle path for the leaf.
-    The Merkle path of a node represents the nodes affected by a change to the node.
-
-    Given a node N(level, index), its parent is N(level-1, floor(index/2)).
-    For every node N(level, index), it has two children: N(level+1, index*2) and N(level+1, index*2+1).
-    Thus, to compute the Merkle path of a node, we recursively list out its ancestors.
-
-    Parameters:
-    - level (int): The level of the node in the Merkle tree.
-    - index (int): The index of the node at the given level.
-
-    Returns:
-    list: A list of dictionaries containing the level and index of each node in the Merkle path excluding the root.
-    """
-    merkle_path = []
-    current_level = level
-    while current_level > 0:
-        merkle_path.append({"level": current_level, "index": index})
-        index //= 2
-        current_level -= 1
-    return merkle_path
-
-
-def get_sibling_node(level, index):
-    if level == 0:
-        raise ValueError("The root does not have a sibling")
-    elif index % 2 == 0:
-        # if node is even, its sibling is at index+1
-        return {"level": level, "index": index + 1}
-    else:
-        # if node is odd, its sibling is at index-1
-        return {"level": level, "index": index - 1}
-
-
 class MerkleTree:
     def __init__(self, height, leaves):
         self.height = height
         self.leaves = leaves
+
+    @staticmethod
+    def hash(left_node, right_node):
+        hasher = hashlib.sha256()
+        concatenated_nodes = f"{left_node}{right_node}".encode("utf-8")
+        hasher.update(concatenated_nodes)
+        return hasher.hexdigest()
+
+    @staticmethod
+    def get_merkle_path_of_node(level, index):
+        """
+        Computes the Merkle path of a node in a Merkle tree.
+
+        When a leaf in a Merkle tree is updated, the change bubbles up the tree
+        affecting all ancestor nodes. These nodes form the Merkle path for the leaf.
+        The Merkle path of a node represents the nodes affected by a change to the node.
+
+        Given a node N(level, index), its parent is N(level-1, floor(index/2)).
+        For every node N(level, index), it has two children: N(level+1, index*2) and N(level+1, index*2+1).
+        Thus, to compute the Merkle path of a node, we recursively list out its ancestors.
+
+        Parameters:
+        - level (int): The level of the node in the Merkle tree.
+        - index (int): The index of the node at the given level.
+
+        Returns:
+        list: A list of dictionaries containing the level and index of each node in the Merkle path excluding the root.
+        """
+        merkle_path = []
+        current_level = level
+        while current_level > 0:
+            merkle_path.append({"level": current_level, "index": index})
+            index //= 2
+            current_level -= 1
+        return merkle_path
+
+    @staticmethod
+    def get_sibling_node(level, index):
+        if level == 0:
+            raise ValueError("The root does not have a sibling")
+        elif index % 2 == 0:
+            # if node is even, its sibling is at index+1
+            return {"level": level, "index": index + 1}
+        else:
+            # if node is odd, its sibling is at index-1
+            return {"level": level, "index": index - 1}
 
     def node(self, level, index):
         """
@@ -87,9 +87,51 @@ class MerkleTree:
         else:
             # if the node is not a leaf, use:
             # N(level, index) = Hash(N(level+1, index*2), N(level+1, index*2+1))
-            return hash_nodes(
+            return self.hash(
                 self.node(level + 1, 2 * index), self.node(level + 1, 2 * index + 1)
             )
 
     def root(self):
         return self.node(0, 0)
+
+    def get_merkle_proof(self, level, index):
+        """
+        Generate a merkle proof for a given leaf node.
+
+        A merkle proof consists of the following components:
+        - The root of the Merkle tree
+        - The siblings of the leaf's merkle path
+        - The index of the leaf in the tree
+        - The value of the leaf
+
+        Using these components, it is possible to prove the inclusion of a certain leaf in a Merkle tree with a known root.
+
+        Parameters:
+        - level (int): The level of the leaf node in the tree.
+        - index (int): The index of the leaf node at the given level.
+
+        Returns:
+        dict: A dictionary containing the components of the merkle proof.
+        """
+        # get the value of the leaf node
+        leaf_value = self.node(level, index)
+
+        # get the levels and indexes of the nodes on the leaf's merkle path
+        merkle_path = self.get_merkle_path_of_node(level, index)
+
+        # get the levels and indexes of the siblings of the nodes on the merkle path
+        merkle_path_siblings = [
+            self.get_sibling_node(node["level"], node["index"]) for node in merkle_path
+        ]
+
+        # get the values of the sibling nodes
+        sibling_values = [
+            self.node(node["level"], node["index"]) for node in merkle_path_siblings
+        ]
+
+        return {
+            "root": self.root(),  # the root we claim to be our tree's root
+            "siblings": sibling_values,  # the siblings of our leaf's merkle path
+            "index": index,  # the index of our leaf
+            "value": leaf_value,  # the value of our leaf
+        }
--- test_merkle_tree.py
+++ test_merkle_tree.py
@@ -1,17 +1,5 @@
-# test_merkle_tree.py
-
 import unittest
-from merkle_tree import MerkleTree, get_merkle_path_of_node
-
-
-class TestMerklePath(unittest.TestCase):
-    def test_merkle_path(self):
-        expected_path = [
-            {"level": 3, "index": 5},
-            {"level": 2, "index": 2},
-            {"level": 1, "index": 1},
-        ]
-        self.assertEqual(get_merkle_path_of_node(3, 5), expected_path)
+from merkle_tree import MerkleTree
 
 
 class TestMerkleTree(unittest.TestCase):
@@ -33,6 +21,42 @@ class TestMerkleTree(unittest.TestCase):
         leaves_2 = leaves_1[:-1] + [1]
 
         self.assertNotEqual(MerkleTree(height, leaves_1), MerkleTree(height, leaves_2))
+
+    def test_merkle_path(self):
+        height = 3
+        tree = MerkleTree(height, list(range(9)))
+        expected_path = [
+            {"level": 3, "index": 5},
+            {"level": 2, "index": 2},
+            {"level": 1, "index": 1},
+        ]
+        self.assertEqual(tree.get_merkle_path_of_node(3, 5), expected_path)
+
+    def test_get_merkle_proof(self):
+        leaves = [1, 3, 3, 7, 4, 2, 0, 6]
+        height = 3
+
+        tree = MerkleTree(height, leaves)
+        leaf_level = 3
+        leaf_index = 5
+        merkle_proof = tree.get_merkle_proof(leaf_level, leaf_index)
+
+        # Check the root
+        self.assertEqual(merkle_proof["root"], tree.root())
+
+        # Verify the leaf index
+        self.assertEqual(merkle_proof["index"], leaf_index)
+
+        # Check the leaf value
+        self.assertEqual(merkle_proof["value"], leaves[leaf_index])
+
+        # Verify the siblings of the leaf's merkle path
+        expected_siblings = [
+            tree.node(3, 4),  # sibling of N(3,5)
+            tree.node(2, 3),  # sibling of N(2,2)
+            tree.node(1, 0),  # sibling of N(1,1)
+        ]
+        self.assertEqual(merkle_proof["siblings"], expected_siblings)
 
 
 if __name__ == "__main__":
